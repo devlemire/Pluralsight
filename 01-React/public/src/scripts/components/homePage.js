f48d1bf36b9e205aee722776ffaeda08fbea536c
= <webdev.jameslemire@gmail.com>
2017-01-10 19:32:02 -0700

    :construction:

---
+++ 01-React/public/src/scripts/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1> Pluralsight Administration </h1>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
